dafc7c2ba6d79e2954fcd1a12c46bbe90a0b8f81
Emeruche Ikenna <emeruchecole@gmail.com>
2019-07-25 22:36:04 +0100

    Updates to work section

--- src/pages/works.js
+++ src/pages/works.js
@@ -16,6 +16,7 @@ class Works extends React.Component {
     const {github} = data.site.siteMetadata.social
     const works = data.allMarkdownRemark.edges
     const githubProfile = `https://github.com/${github}`
+    const blogRepo = `https://github.com/${github}/myblog`
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -33,7 +34,10 @@ class Works extends React.Component {
               <WorkCard repo={repo} github={github} title={title} description={description} tech={tech} link={link}/>
             )
           })}
-          <p className='mt-4'>Other projects can be found at my <a href={githubProfile}>github graveyard</a>.</p>
+          <div className='mt-4'>
+            <p>The repo for this blog can be found <a href={blogRepo}>here</a>.</p>
+            <p>Other projects can be found at my <a href={githubProfile}>github graveyard</a>.</p>
+          </div>
         </div>
       </Layout>
     )
